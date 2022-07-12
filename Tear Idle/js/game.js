b12284bbd50fd9e6fdeb0a9ef72c9819520c4ec0
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-12 16:43:57 -0300

    COOL

--- Tear Idle/js/game.js
+++ Tear Idle/js/game.js
@@ -2,4 +2,6 @@ function Game() {
     this.tear = new Decimal(0);
     this.tpc = new Decimal(1);
     this.tps = new Decimal(0);
+    this.aLevel = new Decimal(0)
+    this.getA = (level) => new Decimal(level).sqrt()
 }
