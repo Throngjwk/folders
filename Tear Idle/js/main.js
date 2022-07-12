c35b06aff1a5f3255be60e284adba4c233e595f4
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-12 17:00:21 -0300

    awesome

--- Tear Idle/js/main.js
+++ Tear Idle/js/main.js
@@ -1,3 +1,14 @@
 document.getElementById("click").onclick = () => {
     game.tear = game.tear.add(1)
+    document.getElementById("tear").innerHTML = "You Have " + game.tear + " Tear."
 }
+
+function UpdateGameArea() {
+    document.getElementById("sqrt").innerText = game.aLevel
+
+    document.getElementById("tear").innerHTML = "You Have " + game.tear + " Tear."
+}
+
+document.body.style.fontSize = "5.5vh"
+
+setInterval(UpdateGameArea, 100)
