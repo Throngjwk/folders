6c9b64da1658adddd0cb5d068e22f70bfe99b1b3
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-12 16:06:30 -0300

    wrong

--- Tear Idle/js/game.js
+++ Tear Idle/js/game.js
@@ -2,13 +2,4 @@ function Game() {
     this.tear = new Decimal(0);
     this.tpc = new Decimal(1);
     this.tps = new Decimal(0);
-    this.achievements = [
-        new Achievement(0, "You Played!", "go aceepted now.", document.getElementById("achievement1"), true)
-    ]
-}
-function Achievement(order, name, description, id, isUnlocked) {
-    this.name = name;
-    this.description = description;
-    this.isUnlocked = isUnlocked;
-    this.id = id;
 }
