284eef5394025c405222c05336d30c6079a0937b
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-14 14:43:09 -0300

    cool

--- Theory/T1/js/main.js
+++ Theory/T1/js/main.js
@@ -1,6 +1,16 @@
-var c1 = new Decimal(0)
-var c2 = new Decimal(0)
-var c3 = new Decimal(0)
-var c4 = new Decimal(0)
+var c1 = new Decimal(1)
+var c2 = new Decimal(1)
+var c3 = new Decimal(1)
+var c4 = new Decimal(1)
 let version = "v1.0.0"
+var rho = new Decimal(0)
+let tau = new Decimal(0)
+
+function Tick() {
+    rho = rho.add(c1.times(c2))
+}
+
+setInterval(() => {
+    document.getElementById("title").innerHTML = "Theory 1:" + rho + " &rho;"
+}, 1000);
 
