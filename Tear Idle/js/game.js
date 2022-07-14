4894954118bb9cf4faefe4ebd969cf6759a25c97
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-14 14:04:10 -0300

    cool

--- Tear Idle/js/game.js
+++ Tear Idle/js/game.js
@@ -5,6 +5,14 @@ function Game() {
     this.aLevel = new Decimal(0)
     this.getA = (level) => new Decimal(level).sqrt()
     this.cost1 = new Decimal(50)
+    this.styleCost1 = new StyleCost1();
+}
+
+function StyleCost1() {
+    /**
+     * Get Font Family go to fonstruct get new account and new font.
+     */
+    this.fontFamily
 }
 
 game = new Game();
