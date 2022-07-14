1f6bbac336f7a7c179da22100bbabf1a398c7c5a
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-14 14:47:25 -0300

    cool

--- Theory/T1/js/main.js
+++ Theory/T1/js/main.js
@@ -2,15 +2,18 @@ var c1 = new Decimal(1)
 var c2 = new Decimal(1)
 var c3 = new Decimal(1)
 var c4 = new Decimal(1)
-let version = "v1.0.0"
+let version = "1.0.0"
 var rho = new Decimal(0)
 let tau = new Decimal(0)
 
 function Tick() {
     rho = rho.add(c1.times(c2))
+    tau = rho;
 }
 
 setInterval(() => {
     document.getElementById("title").innerHTML = "Theory 1:" + rho + " &rho;"
+    Tick();
+    document.getElementById("rhotau").innerHTML = rho + " &rho;, "  + tau + " &tau;"
 }, 1000);
 
