60f0be5a1665ac9ab1fa9cb872ccb72fd016f7cb
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-14 20:09:25 -0300

    cool

--- Theory/T1/js/main.js
+++ Theory/T1/js/main.js
@@ -2,12 +2,14 @@ var c1 = new Decimal(1)
 var c2 = new Decimal(1)
 var c3 = new Decimal(1)
 var c4 = new Decimal(1)
+var a1 = new Decimal(1)
 let version = "1.0.0"
 var rho = new Decimal(0)
 let tau = new Decimal(0)
 var cost1 = new Decimal(15)
 var cost2 = new Decimal(3e3)
 var cost3 = new Decimal(1e4)
+var cost4 = new Decimal(1e10)
 let expo1 = new Decimal(1);
 
 function Tick() {
@@ -76,6 +78,15 @@ document.getElementById("cost3").onclick = () => {
     }
 }
 
+document.getElementById("cost4").onclick = () => {
+    if (rho.gte(cost4)) {
+        CostsDefine(cost4)
+        cost4 = cost4.mul(new Decimal.pow(2, 8 * Math.log2(10)))
+        c4 = c4.mul(10)
+        document.getElementById("cn4").innerText = cost4;
+    }
+}
+
 document.getElementById("mc1").onclick = () =>{
     if (rho.gte(1e25)) {
         CostsDefine(new Decimal(1e25))
@@ -115,6 +126,13 @@ document.getElementById("mc5").onclick = () =>{
     if (rho.gte(1e12)) {
         CostsDefine(new Decimal(1e12))
         document.getElementById("logterm").style.display = "block";
+    }
+}
+
+document.getElementById("mc6").onclick = () =>{
+    if (rho.gte(1e25)) {
+        CostsDefine(new Decimal(1e25))
+        document.getElementById("c4term").style.display = "block";
     }
 }
 
