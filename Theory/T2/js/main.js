225343ada27b61bf7baaa893ea1ee5a5935356cd
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-15 13:53:53 -0300

    cool

---
+++ Theory/T2/js/main.js
@@ -0,0 +1,11 @@
+var rho = new Decimal(0);
+var tau = new Decimal(0)
+var dotq1 = new Decimal(0)
+var dotq2 = new Decimal(0)
+var dotq3 = new Decimal(0)
+var dotq4 = new Decimal(0)
+var dotr1 = new Decimal(0)
+var dotr2 = new Decimal(0)
+var dotr3 = new Decimal(0)
+var dotr4 = new Decimal(0)
+
