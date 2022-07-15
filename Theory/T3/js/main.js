e39f1ed97a81957704022d11574ad19c0360e9f5
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-15 20:19:38 -0300

    awesome

---
+++ Theory/T3/js/main.js
@@ -0,0 +1,20 @@
+var currency1 = new Decimal(0)
+var currency2 = new Decimal(0)
+var currency3 = new Decimal(0)
+var tau = new Decimal(0)
+var b1 = new Decimal(1)
+var b2 = new Decimal(0)
+var b3 = new Decimal(0)
+
+document.getElementById("cost1").onclick = () => {
+    if (currency1.gte(cost1)) {
+        currency1 = currency1.sub(cost1)
+        currency1 = cost1.times(new Decimal.pow(2, Math.log2(1.18099)))
+        b1 = b1.add(1)
+        document.getElementById("cn1").innerText = cost1;
+    }
+}
+
+setInterval(() => {
+    currnecy1 = currency1.add(b1.times(b2).times(b3))
+}, 1000);
