bb0d16b7e2e5f7ba3fc58e33474b3ded9127d3bb
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-15 15:55:03 -0300

    cool awesome

--- Theory/T2/js/main.js
+++ Theory/T2/js/main.js
@@ -8,7 +8,25 @@ var dotr1 = new Decimal(0)
 var dotr2 = new Decimal(0)
 var dotr3 = new Decimal(0)
 var dotr4 = new Decimal(0)
+var cost1 = new Decimal(10)
+var cost2 = new Decimal(5e3)
+var cost3 = new Decimal(3e25)
+var cost4 = new Decimal(8e50)
+var cost5 = new Decimal(2e6)
+var cost6 = new Decimal(3e9)
+var cost7 = new Decimal(4e25)
+var cost8 = new Decimal(5e50)
+
+document.getElementById("cost1").onclick = () => {
+    if (rho.gte(cost1)) {
+        rho = rho.sub(cost1)
+        dotq1 = dotq1.add(1)
+        document.getElementById("cn1").innerText = cost1;
+    }
+}
 
 setInterval(() => {
+    rho = rho.add(dotq1)
+    dotq1 = dotq1.add(dotq2)
     document.getElementById("rhotau").innerHTML = rho + " &rho;, "  + tau + " &tau;"
-}, 100);
+}, 1000);
