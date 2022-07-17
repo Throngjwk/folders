997512fe0a4e0f80c2b7b60e7700f11ef1ddd3cb
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-17 17:17:04 -0300

    COOL

--- Pixel Clicker/js/main.js
+++ Pixel Clicker/js/main.js
@@ -1,10 +1,17 @@
 var pixel = new Decimal(0)
 var pps = new Decimal(0)
 var ppc = new Decimal(1)
+var cost1 = new Decimal(15)
 
 document.getElementById("click").onclick = () => {
     pixel = pixel.add(ppc)
     document.getElementById("pixels").innerText = pixel
+}
+
+document.getElementById("cost1").onclick = () => {
+    pixel = pixel.sub(cost1)
+    cost1 = cost1.times(1.15)
+    pps = pps.add(1)
 }
 
 setInterval(() => {
