f264a3519f8499cc349a2d1e05f5b772380ddb9c
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-19 11:38:23 -0300

    nice

--- Mass Rewittern/js/main.js
+++ Mass Rewittern/js/main.js
@@ -3,3 +3,14 @@ x = document.getElementById("background")
 document.getElementById("setColor").onclick = () => {
     document.body.style.backgroundColor = x.value
 }
+
+x2 = document.getElementById("massChange")
+x3 = document.getElementById("bigMassChange")
+
+document.getElementById("one").onclick = () => {
+   document.getElementById("mass").style.color = x2.value
+}
+
+document.getElementById("two").onclick = () => {
+    document.getElementById("bigMass").style.color = x3.value
+ }
