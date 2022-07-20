314193d7893f5fd04429ec9673fdc527b241ad09
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-20 15:43:36 -0300

    added

---
+++ Mass Rewittern/computed generator/Prod.js
@@ -0,0 +1,22 @@
+import { Computed } from "./computed.js";
+import { SuperNumbers } from "./supernumber.js";
+
+let doc = 0;
+
+Computed.JSON.parse("data")
+Computed.JSON.parse("world")
+
+if (Computed.JSON.parse("find") == false) {
+    if (doc > 0) {
+        define([
+            'require',
+            'documentary'
+        ], function(require, factory) {
+            'use strict';
+            
+        });
+    }
+} else {
+    var dar = SuperNumbers.ONE
+    var every = SuperNumbers.TWO
+}
