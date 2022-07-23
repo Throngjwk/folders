22b39392afaed6811b2d34c48d586ed5dbe04057
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-23 16:58:48 -0300

    gen

--- Others/AtMath+/js/main.js
+++ Others/AtMath+/js/main.js
@@ -1,7 +1,13 @@
 var n = new Decimal(0)
 var t = new Decimal(0)
+var alpha = new Decimal(0)
+var alphaPower = new Decimal(0)
+var alphaPowerReq = new Decimal(25)
+var alphaReqBase = new Decimal(1.45)
 
 setInterval(() => {
+    n = n.add(t.mul(alpha.add(1)))
+    t =t.add(0.1)
    document.getElementById("t").innerText = t
    document.getElementById("nt").innerText = n;
 }, 100);
