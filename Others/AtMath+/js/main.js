75e93cd17eb712d2104f87826c43fe09adb419d7
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-23 21:53:09 -0300

    cool bug

--- Others/AtMath+/js/main.js
+++ Others/AtMath+/js/main.js
@@ -5,9 +5,18 @@ var alphaPower = new Decimal(0)
 var alphaPowerReq = new Decimal(25)
 var alphaReqBase = new Decimal(1.45)
 
+document.getElementsByClassName("reset alpha")[0].onclick = () => {
+    n = new Decimal(0)
+    alphaPower = alphaPower.add(1)
+}
+
 setInterval(() => {
     n = n.add(t.mul(alpha.add(1)))
     t =t.add(0.1)
+    alphaPowerReq = new Decimal.pow(alphaReqBase, alphaPower)
+    alpha = alphaPower
    document.getElementById("t").innerText = t
    document.getElementById("nt").innerText = n;
+   document.getElementById("number1").innerText = alphaPower
+   document.getElementById("number2").innerText = alpha
 }, 100);
