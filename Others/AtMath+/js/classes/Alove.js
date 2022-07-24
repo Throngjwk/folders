0354a309c286241848a6b86787bdf61b2ea218f9
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-24 17:04:34 -0300

    goaled

---
+++ Others/AtMath+/js/classes/Alove.js
@@ -0,0 +1,10 @@
+class Alove {
+    constructor(id) {
+        this.id = id;
+
+        this.template = `
+          <h2 id="aloveLevel">0</h2>
+          <h2 class="alove">Req:n(t) &GreaterEqual; 1e7</h2>
+        `
+    }
+}
