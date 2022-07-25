2ca5b6639924ec5de25d205adcbe694faccbbdf7
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-24 21:38:01 -0300

    fix

--- Others/AtMath+/js/classes/BrickClicker.js
+++ Others/AtMath+/js/classes/BrickClicker.js
@@ -3,7 +3,7 @@ class BrickClicker {
         this.id = id
 
         this.template = `
-          <h1 class="center">You Have <span class="bricks">0</span> Bricks.</h1>
+          <h1 class="center">You Have <span id="bricks">0</span> Bricks.</h1>
           <button id="click">Click Me!</button>
           <button id="brickupg1">Brick Upgrader 1 <br> Increase bricks per click by 1. <br> Cost:<span id="cost4">20</span></button>
         `
