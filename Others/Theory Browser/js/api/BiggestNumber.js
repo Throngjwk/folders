595fcb690a466a92ef6a0a90d9d8bc7f22392321
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-25 17:54:03 -0300

    BIGGEST

---
+++ Others/Theory Browser/js/api/BiggestNumber.js
@@ -0,0 +1,14 @@
+class BiggestNumber {
+    constructor() {
+        this.ONE = new Decimal(1)
+        this.TWO = new Decimal(2)
+        this.THREE = new Decimal(3)
+        this.FOUR = new Decimal(4)
+        this.FIVE = new Decimal(5)
+        this.SIX = new Decimal(6)
+        this.SEVEN = new Decimal(7)
+        this.EIGHT = new Decimal(8)
+        this.NINE = new Decimal(9)
+        this.TEN = new Decimal(10)
+    }
+}
