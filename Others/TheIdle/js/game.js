5fcee71559b7e3b878d31893b4c99b503d2b7000
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-30 00:55:43 -0300

    cool

---
+++ Others/TheIdle/js/game.js
@@ -0,0 +1,8 @@
+var house = new Decimal(0)
+var hpc = new Decimal(0)
+var hg1 = new Decimal(0)
+var hg2 = new Decimal(0)
+var hg3 = new Decimal(0)
+var hg4 = new Decimal(0)
+var hg5 = new Decimal(0)
+var hg6 = new Decimal(0)
