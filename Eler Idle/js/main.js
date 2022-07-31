e731330722bd0cdcad6938c6b060f79e2a503334
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-31 12:00:28 -0300

    cool

--- Eler Idle/js/main.js
+++ Eler Idle/js/main.js
@@ -1,7 +1,10 @@
 var game = {
     coins:new Decimal(0),
     cps:new Decimal(0),
-    cpc:new Decimal(0)
+    cpc:new Decimal(1),
+    clickCoin() {
+        game.coins = game.coins.add(game.cpc)
+    }
 }
 
 const app = new Vue({
