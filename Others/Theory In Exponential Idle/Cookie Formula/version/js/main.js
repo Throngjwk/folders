48bf60d644f7c08959b14ad0f65d6de057077191
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-08-02 23:17:24 -0300

    cool

---
+++ Others/Theory In Exponential Idle/Cookie Formula/version/js/main.js
@@ -0,0 +1,12 @@
+console.log("%cAgain!", "font-family:sans-serif;font-weight:bold;color:darkblue;");
+console.log("Converted than exp idle than be now.");
+
+//Main
+
+function Currency(value, fontSize, label) {
+    this.value = value
+    this.size = fontSize
+    this.label = label
+}
+
+var currency = [new Currency(new Decimal(0), 32, "\u03C1")]
