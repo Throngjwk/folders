49bdb40263de2cd8b2862be15be6ae32c2cc2de1
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-06 09:30:42 -0300

    Update script.js

--- Others/RGBEDColorRainbow/script.js
+++ Others/RGBEDColorRainbow/script.js
@@ -4,6 +4,7 @@ var color3 = ""
 var color4 = ""
 var color5 = ""
 var color6 = ""
+var color7 = ""
 var letters = "0123456789ABCDEF"
 var inputColor = document.getElementById("color")
 
@@ -14,8 +15,9 @@ function randomColor() {
    color4 = "#" + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1]
    color5 = "#" + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1]
    color6 = "#" + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1]
+   color7 = "#" + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1] + letters[Math.floor(Math.random() * 16) - 1]
    document.getElementById("text").innerHTML =
-   '<span style="color:' + color1 + '">' + inputColor.value[0] + '</span>' + '<span style="color:' + color2 + '">' + inputColor.value[1] + '</span>' + '<span style="color:' + color3 + '">' + inputColor.value[2] + '</span>' + '<span style="color:' + color4 + '">' + inputColor.value[3] + '</span>' + '<span style="color:' + color5 + '">' + inputColor.value[4] + '</span>' + '<span style="color:' + color6 + '">' + inputColor.value[5] + '</span>'
+   '<span style="color:' + color1 + '">' + inputColor.value[0] + '</span>' + '<span style="color:' + color2 + '">' + inputColor.value[1] + '</span>' + '<span style="color:' + color3 + '">' + inputColor.value[2] + '</span>' + '<span style="color:' + color4 + '">' + inputColor.value[3] + '</span>' + '<span style="color:' + color5 + '">' + inputColor.value[4] + '</span>' + '<span style="color:' + color6 + '">' + inputColor.value[5] + '</span>' + '<span style="color:' + color7 + '">' + inputColor.value[6] + '</span>'
 }
 
 document.getElementById("gen").onclick = () => {
