11cdeb15311e8b4eb96a2132c6c54c731dcd7680
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-08 13:21:08 -0300

    Update main.js

--- Others/IdlingEater/js/main.js
+++ Others/IdlingEater/js/main.js
@@ -6,7 +6,7 @@ console.log("Note: Than meter be now.")
 var meter = new Decimal(0)
 var meterTools = {
   ps:new Decimal(0),
-  pc:new Decimal(0),
+  pc:new Decimal(1),
   time:0,
   total:0,
   timeLength:0,
@@ -14,9 +14,18 @@ var meterTools = {
     1:{
       title:"Addition Meter by 1",
       level:new Decimal(0),
+      cost:new Decimal(200),
       getEffect() {
-        return meterTools.upgrades["1"].level.add(1)
+        return meterTools.upgrades["1"].level.add(0.5)
       }
     }
   }
 }
+
+function UpdateGameArea() {
+    meter = meter.add(meterTools.upgrades["1"].getEffect())
+    document.getElementById("meter").innerText = meter;
+}
+
+setInterval(UpdateGameArea, 123)
+
