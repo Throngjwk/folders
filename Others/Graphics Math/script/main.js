54e752b7614233caa1c31986ef14849eb86824d6
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-16 18:43:25 -0300

    flexed

--- Others/Graphics Math/script/main.js
+++ Others/Graphics Math/script/main.js
@@ -3,7 +3,14 @@ let levelItems = [
     document.getElementsByClassName("level-item")[0]
 ]
 var buttonItem = []
+var value = [
+    ""
+]
 
 for (let i = 0; i < document.getElementsByClassName("math-addiontial-button").length; i++) {
     buttonItem.push(document.getElementsByClassName("math-addiontial-button")[i])
+}
+
+buttonItem[0].onclick = function() {
+    value[level] += "0"
 }
