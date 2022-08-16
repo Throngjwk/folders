b966755c9592d5326ce9f2be52c0945ee214de35
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-16 18:52:13 -0300

    cool

--- Others/Graphics Math/script/main.js
+++ Others/Graphics Math/script/main.js
@@ -17,6 +17,42 @@ buttonItem[0].onclick = function() {
     value[level] += "0"
 }
 
+buttonItem[1].onclick = function() {
+    value[level] += "1"
+}
+
+buttonItem[2].onclick = function() {
+    value[level] += "2"
+}
+
+buttonItem[3].onclick = function() {
+    value[level] += "3"
+}
+
+buttonItem[4].onclick = function() {
+    value[level] += "4"
+}
+
+buttonItem[5].onclick = function() {
+    value[level] += "5"
+}
+
+buttonItem[6].onclick = function() {
+    value[level] += "6"
+}
+
+buttonItem[7].onclick = function() {
+    value[level] += "7"
+}
+
+buttonItem[8].onclick = function() {
+    value[level] += "8"
+}
+
+buttonItem[9].onclick = function() {
+    value[level] += "9"
+}
+
 setInterval(() => {
     document.getElementById("math-text1").innerText = value[0]
 }, 100);
