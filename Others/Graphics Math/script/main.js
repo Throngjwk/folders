81f094ed6ad316f05ec72035452032d8fc8a4b90
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-16 18:32:35 -0300

    cool

---
+++ Others/Graphics Math/script/main.js
@@ -0,0 +1,9 @@
+var level = 0
+let levelItems = [
+    document.getElementsByClassName("level-item")[0]
+]
+var buttonItem = []
+
+for (let i = 0; i < document.getElementsByClassName("math-addiontial-button").length; i++) {
+    buttonItem.push(document.getElementsByClassName("math-addiontial-button")[i])
+}
